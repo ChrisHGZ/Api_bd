9738c042083e9535898ee54d15e477eacde58a13
Carlos Coronado <coronadoc899@gmail.com>
2025-06-04 13:46:03 -0400

    [ADD]: findAll, findOne, delete

--- src/modules/venta/venta.controller.ts
+++ src/modules/venta/venta.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+} from '@nestjs/common';
 import { VentaService } from './venta.service';
 import { CreateVentaDto } from './dtos/create-venta.dto';
 
@@ -6,8 +14,23 @@ import { CreateVentaDto } from './dtos/create-venta.dto';
 export class VentaController {
   constructor(private readonly _ventaService: VentaService) {}
 
+  @Get()
+  findAll() {
+    return this._ventaService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this._ventaService.findOne(id);
+  }
+
   @Post()
   create(@Body() createVentaDto: CreateVentaDto) {
     return this._ventaService.create(createVentaDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this._ventaService.remove(id);
   }
 }
--- src/modules/venta/venta.service.ts
+++ src/modules/venta/venta.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Venta } from './entity/venta.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -19,69 +23,131 @@ export class VentaService {
   ) {}
 
   public async create(createVentaDto: CreateVentaDto) {
-    const { fecha, nro_boleta, ventaProductos } = createVentaDto;
+    try {
+      const { fecha, nro_boleta, ventaProductos } = createVentaDto;
 
-    if (await this.existeBoleta(nro_boleta)) {
-      throw new BadRequestException(`Boleta ${nro_boleta} ya existe`);
-    }
+      if (await this.existeBoleta(nro_boleta)) {
+        throw new BadRequestException(`Boleta ${nro_boleta} ya existe`);
+      }
 
-    let subMonto = 0;
+      let subMonto: number = 0;
+      const productosCreados = [];
 
-    for (const vp of ventaProductos) {
-      subMonto += vp.precioUnitario * vp.cantidad;
-    }
-
-    const iva = subMonto * 0.19;
-    const montoTotal = subMonto + iva;
-
-    const venta = this._ventaRepository.create({
-      fecha,
-      nro_boleta,
-      subMonto,
-      iva,
-      montoTotal,
-    });
-
-    await this._ventaRepository.save(venta);
-
-    const productosCreados = [];
-    for (const vp of ventaProductos) {
-      const producto = await this._productRepository.findOneByOrFail({
-        id: vp.productoId,
+      const venta = this._ventaRepository.create({
+        fecha,
+        nro_boleta,
+        subMonto: 0,
+        iva: 0,
+        montoTotal: 0,
       });
 
-      const ventaProducto = this._ventaProductoRepository.create({
-        venta,
-        producto,
-        cantidad: vp.cantidad,
-        precioUnitario: vp.precioUnitario,
-        montoTotal: vp.precioUnitario * vp.cantidad,
-      });
+      await this._ventaRepository.save(venta);
 
-      await this._ventaProductoRepository.save(ventaProducto);
+      for (const vp of ventaProductos) {
+        const producto = await this._productRepository.findOneByOrFail({
+          id: vp.productoId,
+        });
 
-      producto.stock -= vp.cantidad;
-      await this._productRepository.save(producto);
+        const precioUnitario = producto.precio;
+        const montoTotalProducto = precioUnitario * vp.cantidad;
+        subMonto += montoTotalProducto;
 
-      productosCreados.push({
-        productoId: producto.id,
-        codigo: producto.codigo,
-        nombre: producto.nombre,
-        cantidad: vp.cantidad,
-        precioUnitario: vp.precioUnitario,
-        montoTotal: vp.precioUnitario * vp.cantidad,
-      });
+        const ventaProducto = this._ventaProductoRepository.create({
+          venta,
+          producto,
+          cantidad: vp.cantidad,
+          precioUnitario,
+          montoTotal: montoTotalProducto,
+        });
+
+        await this._ventaProductoRepository.save(ventaProducto);
+
+        producto.stock -= vp.cantidad;
+        await this._productRepository.save(producto);
+
+        productosCreados.push({
+          productId: producto.id,
+          codigo: producto.codigo,
+          nombre: producto.nombre,
+          cantidad: vp.cantidad,
+          precioUnitario,
+          montoTotal: montoTotalProducto,
+        });
+      }
+
+      const iva = subMonto * 0.19;
+      const montoTotal = subMonto + iva;
+
+      venta.subMonto = subMonto;
+      venta.iva = iva;
+      venta.montoTotal = montoTotal;
+
+      await this._ventaRepository.save(venta);
+
+      return {
+        id: venta.id,
+        fecha: venta.fecha,
+        nro_boleta: venta.nro_boleta,
+        subMonto,
+        iva,
+        montoTotal,
+        productos: productosCreados,
+      };
+    } catch (error) {
+      this.handleDBErrors(error);
     }
+  }
 
-    return {
-      id: venta.id,
-      fecha: venta.fecha,
-      nro_boleta: venta.nro_boleta,
-      subMonto: venta.subMonto,
-      iva: venta.iva,
-      montoTotal: venta.montoTotal,
-      productos: productosCreados,
-    };
+  public async findAll() {
+    const queryBuilder = this._ventaRepository.createQueryBuilder('venta');
+
+    const venta = await queryBuilder
+      .leftJoinAndSelect('venta.VentaProductos', 'ventaProductos')
+      .leftJoinAndSelect('ventaProductos.producto', 'producto')
+      .where('venta.status = :status', { status: status.ACTIVE })
+      .getMany();
+
+    if (!venta) throw new BadRequestException(`No existen ventas`);
+
+    return venta;
+  }
+
+  public async findOne(id: string) {
+    const queryBuilder = this._ventaRepository.createQueryBuilder('venta');
+
+    const venta = await queryBuilder
+      .leftJoinAndSelect('venta.VentaProductos', 'ventaProductos')
+      .leftJoinAndSelect('ventaProductos.producto', 'producto')
+      .where('venta.id = :id', { id })
+      .andWhere('venta.status = :status', { status: status.ACTIVE })
+      .getOne();
+
+    if (!venta) throw new BadRequestException('No existe la venta');
+
+    return venta;
+  }
+
+  public async remove(id: string) {
+    const venta = await this.findOne(id);
+
+    if (!venta) throw new BadRequestException('No existe la venta');
+
+    try {
+      venta.status = status.INACTIVE;
+      await this._ventaRepository.save(venta);
+
+      for (const vp of venta.VentaProductos) {
+        vp.producto.stock += vp.cantidad;
+        await this._productRepository.save(vp.producto);
+
+        vp.status = status.INACTIVE;
+        await this._ventaProductoRepository.save(vp);
+      }
+
+      return { message: 'Venta anulada' };
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
   }
 
   private existeBoleta(nro_boleta: number) {
@@ -89,5 +155,19 @@ export class VentaService {
       nro_boleta: nro_boleta,
       status: status.ACTIVE,
     });
+  }
+
+  private handleDBErrors(error: any) {
+    if (error.code === 'ER_DUP_ENTRY') {
+      throw new BadRequestException(error.sqlMessage);
+    }
+
+    if (error) {
+      throw new BadRequestException(error);
+    }
+
+    throw new InternalServerErrorException(
+      'Unexpected error, check server logs',
+    );
   }
 }
